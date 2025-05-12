9799c1f9b6e965acd49d8a80334a3ea2cd0d761e
Sam Gubernikoff <sgubernikoff@gmail.com>
2025-05-12 10:51:23 -0400

    update backend

---
+++ frontend/remix.config.js
@@ -0,0 +1,8 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+export default {
+  appDirectory: "app",
+  assetsBuildDirectory: "public/build",
+  publicPath: "/build/",
+  serverBuildTarget: "vercel",
+  ignoredRouteFiles: ["**/.*"],
+};
