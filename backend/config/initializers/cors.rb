c675bb376c4f41c25ce16f40165d4da1b88fd7c4
Sam Gubernikoff <sgubernikoff@gmail.com>
2025-05-12 12:42:00 -0400

    update cors

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['http://localhost:5173', 'https://your-frontend-domain.com']
+    origins ['http://localhost:5173', 'https://df-pf.vercel.app']
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
