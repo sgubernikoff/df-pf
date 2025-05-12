1e207e5442b8a892ddf0d15316b9100086605ec1
Sam Gubernikoff <sgubernikoff@gmail.com>
2025-05-12 15:49:30 -0400

    idiot sam IDIOT

--- frontend/app/routes/user-search.jsx
+++ frontend/app/routes/user-search.jsx
@@ -21,7 +21,7 @@ export async function loader({ request }) {
   const query = url.searchParams.get("query") || "";
 
   const res = await fetch(
-    `https://df-pf.vercel.app/users/search?query=${query}`,
+    `https://df-pf.onrender.com/users/search?query=${query}`,
     {
       headers: {
         Authorization: token,
