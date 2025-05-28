b371a574880f881fd0dd95d93f46729475b8c5ae
Sam Gubernikoff <sgubernikoff@gmail.com>
2025-05-27 20:30:29 -0400

    error fix

--- backend/app/jobs/generate_visit_pdf_job.rb
+++ backend/app/jobs/generate_visit_pdf_job.rb
@@ -4,11 +4,14 @@ class GenerateVisitPdfJob < ApplicationJob
   def perform(visit_id)
     visit = Visit.find(visit_id)
     password = visit.generate_pdf_and_store
-    
+
     if password
-      NotificationMailer.job_completion_email(visit.user, visit.id, password).deliver_later
+      begin
+        NotificationMailer.job_completion_email(visit.user, visit.id, password).deliver_later
+      rescue Net::ReadTimeout => e
+        Rails.logger.warn("Email likely sent, but SMTP response timed out: #{e.message}")
+      end
     else
-      # Handle failure case
       Rails.logger.error("Failed to generate PDF for Visit #{visit_id}")
     end
   end
