0a2e26aceb6f75d499e656d260d4c1e7fe35e004
Sam Gubernikoff <sgubernikoff@gmail.com>
2025-05-27 20:21:35 -0400

    remove mailer error

--- backend/app/jobs/generate_visit_pdf_job.rb
+++ backend/app/jobs/generate_visit_pdf_job.rb
@@ -6,7 +6,7 @@ class GenerateVisitPdfJob < ApplicationJob
     password = visit.generate_pdf_and_store
     
     if password
-      NotificationMailer.job_completion_email(visit.user, visit.id, password).deliver_now
+      NotificationMailer.job_completion_email(visit.user, visit.id, password).deliver_later
     else
       # Handle failure case
       Rails.logger.error("Failed to generate PDF for Visit #{visit_id}")
