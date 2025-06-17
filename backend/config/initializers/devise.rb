666d23c0af0944e316184be1bb00d6ae0e444562
Sam Gubernikoff <sgubernikoff@gmail.com>
2025-06-17 12:59:13 -0400

    update mailer

--- backend/config/initializers/devise.rb
+++ backend/config/initializers/devise.rb
@@ -30,7 +30,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'no-reply@yourdomain.com'
+  config.mailer_sender = 'sgubernikoff@gmail.com'
 
 
   # Configure the parent class responsible to send e-mails.
