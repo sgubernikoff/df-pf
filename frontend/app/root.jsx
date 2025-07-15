87c89baaa3247057e8e7f3202f3a2ff1ca63c126
Sam Gubernikoff <sgubernikoff@gmail.com>
2025-07-14 23:09:26 -0400

    favicon

--- frontend/app/root.jsx
+++ frontend/app/root.jsx
@@ -11,6 +11,7 @@ import Header from "./components/Header";
 import { useLoaderData, useLocation } from "@remix-run/react";
 import { AuthProvider } from "./context/auth";
 import ProtectedRoutes from "./components/ProtectedRoutes";
+import favicon from "./styles/favicon.png";
 
 // Loader function to get user data based on session
 // export async function loader({ request }) {
@@ -42,6 +43,7 @@ export function links() {
       rel: "stylesheet",
       href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
     },
+    { rel: "icon", type: "image/jpg", href: favicon },
   ];
 }
 
